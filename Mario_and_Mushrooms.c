15c5923c17d7d647d2eaadf1c75b37cdfebdba1c
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-10-20 20:42:42 +0530

    #include<stdio.h> int main() {     int x,i,f;     scanf("%d",&x);     i=1;     f=(x+i)%3;     if(f==0) printf("SMALL");     else if(f==1) printf("NORMAL");     else printf("HUGE"); }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -4,8 +4,8 @@ int main()
     int x,i,f;
     scanf("%d",&x);
     i=1;
-    f=(i+x)%3;
-    if (f==0) printf("SMALL");
-    else if (f==1) printf("NORMAL");
+    f=(x+i)%3;
+    if(f==0) printf("SMALL");
+    else if(f==1) printf("NORMAL");
     else printf("HUGE");
 }
