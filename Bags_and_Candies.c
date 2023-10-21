6b664fe5bf72554cc7e530bba0fc1bbfa559a4ca
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-10-21 21:15:53 +0530

    #include<stdio.h> int main() {     int n, k, m;     scanf("%d%d%d",&n,&k,&m);     int a=n/(k*m);     if (n%(k*m)==0) printf("%d",a);     else printf("%d",a+1); }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,m;
+    int n, k, m;
     scanf("%d%d%d",&n,&k,&m);
-    if (n%(k*m)==0) printf("%d",n/(k*m));
-    else printf("%d",(n/(k*m))+1);
+    int a=n/(k*m);
+    if (n%(k*m)==0) printf("%d",a);
+    else printf("%d",a+1);
 }
