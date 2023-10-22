508782d5100e41d2f2598990f965dc98064f5338
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-10-22 12:17:41 +0530

    #include<stdio.h> int main() {     float a;     scanf("%f",&a);     if (a<=199)     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 1.20 ");         printf("Bill: %.2f ",a*1.20);         printf("Surcharge: 0.00 ");         printf("Total Amount: %.2f ",a*1.20);     }     else if (a>=200 && a<400 && a*1.40<=400)     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 1.40 ");         printf("Bill: %.2f ",a*1.40);         printf("Surcharge: 0.00 ");         printf("Total Amount: %.2f ",a*1.40);     }     else if (a>=200 && a<400 && a*1.40>400)     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 1.40 ");         printf("Bill: %.2f ",a*1.40);         printf("Surcharge: %.2f ",a*1.40*0.15);         printf("Total Amount: %.2f ",a*1.40+a*1.40*0.15);     }     else if (a>=400 && a<600)     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 1.60 ");         printf("Bill: %.2f ",a*1.60);         printf("Surcharge: %.2f ",a*1.60*0.15);         printf("Total Amount: %.2f ",a*1.60+a*1.60*0.15);     }     else if (a>=600 && a<800)     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 1.80 ");         printf("Bill: %.2f ",a*1.80);         printf("Surcharge: %.2f ",a*1.80*0.15);         printf("Total Amount: %.2f ",a*1.80+a*1.80*0.15);     }     else     {         printf("Units Consumed: %.0f ",a);         printf("Cost per Unit: 2.00 ");         printf("Bill: %.2f ",a*2.00);         printf("Surcharge: %.2f ",a*2.00*0.15);         printf("Total Amount: %.2f ",a*2.00+a*2.00*0.15);     } }

---
+++ Electricity_Bill.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    if (a<=199)
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 1.20
+");
+        printf("Bill: %.2f
+",a*1.20);
+        printf("Surcharge: 0.00
+");
+        printf("Total Amount: %.2f
+",a*1.20);
+    }
+    else if (a>=200 && a<400 && a*1.40<=400)
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 1.40
+");
+        printf("Bill: %.2f
+",a*1.40);
+        printf("Surcharge: 0.00
+");
+        printf("Total Amount: %.2f
+",a*1.40);
+    }
+    else if (a>=200 && a<400 && a*1.40>400)
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 1.40
+");
+        printf("Bill: %.2f
+",a*1.40);
+        printf("Surcharge: %.2f
+",a*1.40*0.15);
+        printf("Total Amount: %.2f
+",a*1.40+a*1.40*0.15);
+    }
+    else if (a>=400 && a<600)
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 1.60
+");
+        printf("Bill: %.2f
+",a*1.60);
+        printf("Surcharge: %.2f
+",a*1.60*0.15);
+        printf("Total Amount: %.2f
+",a*1.60+a*1.60*0.15); 
+    }
+    else if (a>=600 && a<800)
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 1.80
+");
+        printf("Bill: %.2f
+",a*1.80);
+        printf("Surcharge: %.2f
+",a*1.80*0.15);
+        printf("Total Amount: %.2f
+",a*1.80+a*1.80*0.15);
+    }
+    else
+    {
+        printf("Units Consumed: %.0f
+",a);
+        printf("Cost per Unit: 2.00
+");
+        printf("Bill: %.2f
+",a*2.00);
+        printf("Surcharge: %.2f
+",a*2.00*0.15);
+        printf("Total Amount: %.2f
+",a*2.00+a*2.00*0.15);
+    }
+}
