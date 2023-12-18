4a241ecdca54ea5d91b7e92a6b869377878a6c51
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-12-18 21:10:13 +0530

    #include<stdio.h> int main() {     int n,i,aver=0,elem=0;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);         aver+=arr[i];     }     aver/=n;     for (i=0;i<n;i++)     {         if (arr[i]<=aver)         {             elem++;         }     }     printf("%d",elem); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,aver=0,elem=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        aver+=arr[i];
+    }
+    aver/=n;
+    for (i=0;i<n;i++)
+    {
+        if (arr[i]<=aver)
+        {
+            elem++;
+        }
+    }
+    printf("%d",elem);
+}
