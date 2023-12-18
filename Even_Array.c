bad9bc6e64f57ec670eb314df4806d4d8529f335
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-12-18 21:03:44 +0530

    #include<stdio.h> int main() {     int n,i,even=1;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for (i=0;i<n;i++)     {         if (arr[i]%2!=0)         {             even=0;             break;         }     }     if (even==1) printf("True");     else printf("False"); }

---
+++ Even_Array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,even=1;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if (arr[i]%2!=0)
+        {
+            even=0;
+            break;
+        }
+    }
+    if (even==1) printf("True");
+    else printf("False");
+}
