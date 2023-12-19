486800373bd82e2fefb8c445b081bb8d84d62f87
mjkrupal <147424148+mjkrupal@users.noreply.github.com>
2023-12-19 11:00:10 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int c=0;     for (i=0;i<n;i++)     {         if (arr[i]%2==1 )         {             for (i=2;i<n;i++)             {                 if (arr[i]%2==1 )                 {                     c++;                 }             }         }     }     printf("%d",c); }

---
+++ Count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0;
+    for (i=0;i<n;i++)
+    {
+        if (arr[i]%2==1 )
+        {
+            for (i=2;i<n;i++)
+            {
+                if (arr[i]%2==1 )
+                {
+                    c++;
+                }
+            }
+        }
+    }
+    printf("%d",c);
+}
